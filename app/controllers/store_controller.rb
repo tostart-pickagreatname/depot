c03a168ae3384d68a05355850863bab895b357c3
Steven Scott <steven.scott@shopify.com>
2016-01-05 14:46:24 -0500

    Gave index nicer style.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,4 +1,7 @@
 class StoreController < ApplicationController
   def index
+    # here we reference it by using the name of the class of the objects.
+    # then you call a class method of order, grabbing the order by the title.
+    @products = Product.order(:title)
   end
 end
